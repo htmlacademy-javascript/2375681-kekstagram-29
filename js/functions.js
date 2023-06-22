3f49bd4dc8e5f7ffd1f82a8643839ac280070b54
KonstantinButko <butko.1985@icloud.com>
2023-06-22 17:25:54 +0300

    ДЗ 5.14. Модуляция

--- js/functions.js
+++ js/functions.js
@@ -14,20 +14,7 @@ checkStrLength('Программа', 11);
 
 // Второе задание
 
-const isPalindrome = (string) => string.toUpperCase() === string.toUpperCase().split('').reverse().join('');
-
-isPalindrome('Лёша на полке клопа нашёл ');
 
 
-function findPalindrome(string) {
-  string = string.replaceAll().toLowerCase();
-  for (let i = string.length - 1; i >= 0; i -= 1) {
-    if (string[i] !== string[string.length - i - 1]) {
-      return false;
-    }
-  }
-  return true;
 
-}
-
-findPalindrome('Лёша на полке клопа нашёл ');
+export {checkStringLength, checkStrLength};
