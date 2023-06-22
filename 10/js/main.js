61d572a3feca87ac0c66a3d73f977afb06c3d4f8
Keks <keks@htmlacademy.ru>
2023-06-22 17:37:53 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,28 @@
+import {getRandomInteger} from './util';
+import {generateComments, addPhotos, addPhoto} from './data';
+
+
+
+
+
+
+const isPalindrome = (string) => string.toUpperCase() === string.toUpperCase().split('').reverse().join('');
+
+isPalindrome('Лёша на полке клопа нашёл ');
+
+
+function findPalindrome(string) {
+  string = string.replaceAll().toLowerCase();
+  for (let i = string.length - 1; i >= 0; i -= 1) {
+    if (string[i] !== string[string.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+
+}
+
+findPalindrome('Лёша на полке клопа нашёл ');
+
+
+// export {findPalindrome};
