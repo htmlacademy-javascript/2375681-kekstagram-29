9f59c399839a7cb7ffb6eb9f2665d0534c426b1f
Keks <keks@htmlacademy.ru>
2023-06-28 00:34:49 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,9 @@
+
+import {addPhotos, addPhoto} from './data';
+
+
+const isPalindrome = (string) => string.toUpperCase() === string.toUpperCase().split('').reverse().join('');
+
+isPalindrome('Лёша на полке клопа нашёл ');
+
+
---
+++ 12/js/util.js
@@ -0,0 +1,5 @@
+
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+export {getRandomInteger};
