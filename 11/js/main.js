3590b9c85cd63ebd589e1f923c2d290d75e1228a
Keks <keks@htmlacademy.ru>
2023-06-27 19:42:09 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/main.js
+++ 11/js/main.js
@@ -1,26 +1,8 @@
 import {generateComments, addPhotos, addPhoto} from './data';
 
 
-
-
-
-
 const isPalindrome = (string) => string.toUpperCase() === string.toUpperCase().split('').reverse().join('');
 
 isPalindrome('Лёша на полке клопа нашёл ');
-
-
-function findPalindrome(string) {
-  string = string.replaceAll().toLowerCase();
-  for (let i = string.length - 1; i >= 0; i -= 1) {
-    if (string[i] !== string[string.length - i - 1]) {
-      return false;
-    }
-  }
-  return true;
-
-}
-
-findPalindrome('Лёша на полке клопа нашёл ');
 
 
