0bbf5d327e9d148b56ff4a4efc4d5800bec27574
KonstantinButko <butko.1985@icloud.com>
2023-07-23 17:57:08 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {photos} from './data.js';
 import {renderPhotos} from './pictures.js';
-import {onImageUploadFormChange, closeForm} from './form/validate.js';
+import {onImageUploadFormChange, closeForm} from './form-validate.js';
 
 
 renderPhotos(photos);
