16bc2e2b126d2815e74456c36d014172830e3b7c
Keks <keks@htmlacademy.ru>
2023-07-26 18:55:55 +0300

    :heavy_check_mark: Сборка #20

---
+++ 20/js/big-picture.js
@@ -0,0 +1,94 @@
+
+const COMMENTS_STEP = 5;
+
+const bigPhoto = document.querySelector('.big-picture');
+const bigPhotoImg = bigPhoto.querySelector('.big-picture__img');
+const likesCount = bigPhoto.querySelector('.likes-count');
+const photoCaption = bigPhoto.querySelector('.social__caption');
+const bigPhotoCloseButton = bigPhoto.querySelector('.big-picture__cancel');
+const socialComments = bigPhoto.querySelector('.social__comments');
+const loadComments = bigPhoto.querySelector('.comments-loader');
+const socialFooterText = bigPhoto.querySelector('.social__footer-text');
+const commentItem = socialComments.querySelector('.social__comment');
+
+let commentsCount = COMMENTS_STEP;
+let currentComments = [];
+
+const renderComments = () => {
+  socialComments.innerHTML = '';
+
+  commentsCount = (commentsCount > currentComments.length) ? currentComments.length : commentsCount;
+
+  const commentsSelected = currentComments.slice(0, commentsCount);
+
+  if (currentComments.length <= COMMENTS_STEP || commentsCount >= currentComments.length){
+    loadComments.classList.add('hidden');
+  }else{
+    loadComments.classList.remove('hidden');
+  }
+
+  socialComments.textContent = `${commentsCount} из ${currentComments.length} комментариев`;
+
+  const commentsFragment = document.createDocumentFragment();
+
+  commentsSelected.forEach((comment) => {
+    const newComment = commentItem.cloneNode(true);
+    newComment.querySelector('.social__picture').src = comment.avatar;
+    newComment.querySelector('.social__picture').alt = comment.name;
+    newComment.querySelector('.social__text').textContent = comment.message;
+
+    commentsFragment.appendChild(newComment);
+  });
+
+  socialComments.appendChild(commentsFragment);
+};
+
+
+const onLoadCommentsButtonClick = () => {
+  commentsCount += COMMENTS_STEP;
+  renderComments();
+};
+
+const showBigPhoto = (photo) => {
+  const {url, likes, comments, description} = photo;
+  bigPhoto.classList.remove ('hidden');
+  document.body.classList.add ('modal-open');
+
+  bigPhotoImg.src = url;
+  likesCount .textContent = likes;
+  photoCaption.textContent = description;
+
+  currentComments = comments.slice();
+
+  renderComments();
+
+  const closeBigPhoto = () => {
+    bigPhoto.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    commentsCount = COMMENTS_STEP;
+    currentComments = [];
+    socialFooterText.value = '';
+
+    loadComments.removeEventListener('click', onLoadCommentsButtonClick);
+    document.removeEventListener('keydown', onBigPhotoEscKeyDown);
+    bigPhotoCloseButton.removeEventListener('click', onBigPhotoCloseButtonClick);
+  };
+
+
+  function onBigPhotoEscKeyDown (evt) {
+    if(evt.key === 'Escape'){
+      closeBigPhoto();
+    }
+  }
+
+
+  function onBigPhotoCloseButtonClick () {
+    closeBigPhoto();
+  }
+
+  loadComments.addEventListener('click', onLoadCommentsButtonClick);
+  document.addEventListener('keydown',onBigPhotoEscKeyDown);
+  bigPhotoCloseButton.addEventListener('click', onBigPhotoCloseButtonClick);
+};
+
+export {showBigPhoto};
---
+++ 20/js/pictures.js
@@ -0,0 +1,35 @@
+import {showBigPhoto} from './big-picture.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const templateFragment = document.querySelector('#picture').content.querySelector('.picture');
+
+
+const renderPhoto = (photo) => {
+  const {url, comments, likes, description} = photo;
+  const element = templateFragment.cloneNode(true);
+  element.querySelector('.picture__img').src = url;
+  element.querySelector('.picture__img').alt = description;
+  element.querySelector('.picture__comments').textContent = comments.length;
+  element.querySelector('.picture__likes').textContent = likes;
+
+  const onElementClick = (evt) => {
+    evt.preventDefault();
+    showBigPhoto(photo);
+  };
+
+  element.addEventListener ('click', onElementClick);
+  return element;
+};
+
+const renderPhotos = (photos) => {
+
+  const fragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    fragment.appendChild(renderPhoto(photo));
+  });
+
+  picturesContainer.appendChild(fragment);
+};
+
+
+export {renderPhotos};
