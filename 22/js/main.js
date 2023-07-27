46f4079349031fa9c42fc51be3264d950cc18f13
Keks <keks@htmlacademy.ru>
2023-07-27 22:53:21 +0300

    :heavy_check_mark: Сборка #22

---
+++ 22/js/main.js
@@ -0,0 +1,33 @@
+// import {photos} from './data.js';
+import {renderPhotos} from './pictures.js';
+import './form-validate.js';
+import './scale.js';
+import './effects.js';
+import {loadData} from './fetch.js';
+
+
+let photos = [];
+
+const onSuccess = (data) => {
+  photos = data.slice();
+  renderPhotos(photos);
+};
+
+const errorAlert = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.zIndex = '100';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.textAlign = 'center';
+  messageAlert.style.fontSize = '22px';
+  messageAlert.style.backgroundColor = 'orange';
+  messageAlert.textContent = 'Ошибка загрузки!';
+  document.body.append(messageAlert);
+};
+
+loadData(onSuccess, errorAlert);
+
+
+renderPhotos(photos);
